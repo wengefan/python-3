81e03e2e2cc22482762aea34c7806401c4501dc8
wengefan <fwg1126@gmail.com>
2018-01-19 17:40:13 +0900

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -12,21 +12,21 @@ binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary,do_not)
 
-print x
-print y
+print (x);
+print (y);
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
+print ("I said: %r." % x);
+print ("I also said: '%s'." % y);
 
 hilarious = False # 爆笑
 joke_evaluation = "Isn't that joke so funny?! %r"# 笑话评估
 
-print joke_evaluation % hilarious
+print (joke_evaluation % hilarious);
 
 w = "This is the left side of....."
 e = "a string with a right side."
 
-print w + e
+print (w + e);
 
 # 笔记
 #
